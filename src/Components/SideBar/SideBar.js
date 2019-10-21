227fbbbe61205eaf00d943eb638ed93be25a928e
OlgaSavytska <o.savytska77@gmail.com>
2019-10-21 10:59:59 +0300

    sidebar2

--- src/Components/SideBar/SideBar.js
+++ src/Components/SideBar/SideBar.js
@@ -9,7 +9,17 @@ const SideBar = () => {
         <div className={s.sidebar_logo_section}>
           <img className={s.sidebar_logo_img} src={logo} alt="Logo" />
         </div>
+        <div className={s.sidebar_name_section}>
+          <p className={s.sidebar_name}>
+            Онлайн сервіс подання {'\n'} Декларацій
+          </p>
+        </div>
       </div>
+      <ul className={s.sidebar_items}>
+        <li className={s.sidebar_item}>Анкета</li>
+        <li className={s.sidebar_item}>Перелік документів</li>
+        <li className={s.sidebar_item}>Декларація</li>
+      </ul>
     </div>
   );
 };
