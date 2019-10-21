665ddf4544d3a8ae86d5bc421e6b0ac979d8ef35
OlgaSavytska <o.savytska77@gmail.com>
2019-10-21 12:39:58 +0300

    sidebar+declaration1

--- src/Components/SideBar/SideBar.js
+++ src/Components/SideBar/SideBar.js
@@ -1,8 +1,10 @@
+/* eslint-disable react/button-has-type */
 import React from 'react';
 import s from './SideBar.module.css';
 import logo from '../../image/Logo.png';
+import Declaration from '../Declaration/Declaration';
 
-const SideBar = () => {
+const SideBar = openDeclaration => {
   return (
     <div className={s.sidebar_container}>
       <div className={s.sidebar_info}>
@@ -16,9 +18,21 @@ const SideBar = () => {
         </div>
       </div>
       <ul className={s.sidebar_items}>
-        <li className={s.sidebar_item}>Анкета</li>
-        <li className={s.sidebar_item}>Перелік документів</li>
-        <li className={s.sidebar_item}>Декларація</li>
+        <li className={s.sidebar_item}>
+          <button className={s.sidebar_item_button}>Анкета</button>
+        </li>
+        <li className={s.sidebar_item}>
+          <button className={s.sidebar_item_button}>Перелік документів</button>
+        </li>
+        <li className={s.sidebar_item}>
+          <button
+            onClick={() => openDeclaration(Declaration)}
+            type="button"
+            className={s.sidebar_item_button}
+          >
+            Декларація
+          </button>
+        </li>
       </ul>
     </div>
   );
